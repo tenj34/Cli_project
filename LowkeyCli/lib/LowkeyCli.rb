6bc597531a02e153593cebc039000cca8d2042ec
Juan Tenezaca <tenj34@gmail.com>
2018-09-06 01:10:45 +0000

    environment set

--- LowkeyCli/lib/LowkeyCli.rb
+++ LowkeyCli/lib/LowkeyCli.rb
@@ -1,8 +1,11 @@
 #Environment
-
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
 require_relative "./LowkeyCli/version"
 require_relative "./LowkeyCli/cli"
+require_relative "./LowkeyCli/sweatshirt"
 
 module LowKeyCli
   # Your code goes here...
