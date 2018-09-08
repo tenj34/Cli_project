f4fcc5a552295d1f7809d0beaf0d180e59bf3376
Juan Tenezaca <tenj34@gmail.com>
2018-09-08 18:10:25 +0000

    refactoring #2

--- LowkeyCli/lib/LowkeyCli.rb
+++ LowkeyCli/lib/LowkeyCli.rb
@@ -7,6 +7,7 @@ require 'colorize'
 require_relative "./LowkeyCli/version"
 require_relative "./LowkeyCli/cli"
 require_relative "./LowkeyCli/sweatshirt"
+require_relative "./LowkeyCli/scraper.rb"
 
 module LowKeyCli
   # Your code goes here...
--- LowkeyCli/lib/LowkeyCli/sweatshirt.rb
+++ LowkeyCli/lib/LowkeyCli/sweatshirt.rb
@@ -8,31 +8,14 @@ class LowkeyCli::Sweatshirt
     @description = description
   end
 
-
-  def self.today
-    self.scrape_sweatshirt
-  end
-
-  def self.scrape_sweatshirt
+  def self.new_sweathshirt_html(sweatshirt_html)
     # go to Lowkey
     # extract the properties from the first page
     # show info on the sweatshirts
-
-    show_sweatshirt = []
-
-    show_sweatshirt = self.lowkey_scrape_sweatshirt
-
-    show_sweatshirt
-
-  end
-
-  def self.lowkey_scrape_sweatshirt
-    doc = Nokogiri::HTML(open("https://www.lowkey.industries/shop/?category=Sweatshirts"))
-    sweatshirt = self.new
-    sweatshirt.name = doc.css(".ProductList-title")[0].text
-    sweatshirt.price = doc.css(".product-price .sqs-money-native")[0].text
-    sweatshirt.url = doc.css(".ProductList-grid.clear a[href]").attribute("href").value
-
-    sweatshirt
+    self.new(
+      sweatshirt.name = doc.css(".ProductList-title").text
+      sweatshirt.price = doc.css(".product-price").text
+      sweatshirt.url = doc.css(".ProductList-grid.clear a[href]").attribute("href").value
+    )
   end
 end
